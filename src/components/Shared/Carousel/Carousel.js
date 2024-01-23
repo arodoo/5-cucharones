85bf224cd7ffe32c02902eca9dd92e0d78dec47b
arodoo <haro.em@hotmail.com>
2024-01-23 11:54:28 -0600

    Add pagination to Carousel component

--- src/components/Shared/Carousel/Carousel.js
+++ src/components/Shared/Carousel/Carousel.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View } from 'react-native'
 import { Image } from 'react-native-elements'
-import CarouselSnap from 'react-native-snap-carousel'
+import CarouselSnap, {Pagination} from 'react-native-snap-carousel'
+import { size } from 'lodash'
 import { styles } from './Carousel.styles'
 
 export function Carousel(props) {
 
-    const {arrayImages, height, width} = props
+    const {arrayImages, height, width, hideDots} = props
+    const [activeDotIndex, setActiveDotIndex] = useState(0)
 
     const renderItem = ({item}) => {
         return (
             <Image
                 style={{width, height}}
                 source={{uri: item}}
+            />
+        )
+    }
+
+    const pagination = () => {
+        return (
+            <Pagination
+                dotsLength={size(arrayImages)}
+                activeDotIndex={activeDotIndex}
+                containerStyle={styles.containerPagination}
+                dotStyle={styles.dotStyle}
+                inactiveDotOpacity={0.35}
+                inactiveDotScale={0.35}
             />
         )
     }
@@ -25,7 +40,10 @@ export function Carousel(props) {
             sliderWidth={width}
             itemWidth={width}
             renderItem={renderItem}
+            onSnapToItem={(index) => setActiveDotIndex(index)}
         />
+
+        {!hideDots && pagination()}
     </View>
   )
 }
--- src/components/Shared/Carousel/Carousel.styles.js
+++ src/components/Shared/Carousel/Carousel.styles.js
@@ -6,4 +6,18 @@ export const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
+    containerPagination: {
+        backgroundColor: 'transparent',
+        zIndex: 1,
+        position: 'absolute',
+        bottom: 0,
+        alignSelf: 'center',
+    },
+    dotStyle: {
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        marginHorizontal: 2,
+        width: 10,
+        height: 10,
+    },
 });
