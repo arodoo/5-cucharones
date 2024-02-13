42d211e9fd2eaf5a220e9036cdd8e0a3773bea8b
arodoo <haro.em@hotmail.com>
2024-02-13 09:52:38 -0600

    Refactor AddReviewRestaurantScreen component

--- src/screens/Restaurants/AddReviewRestaurantScreeen/AddReviewRestaurantScreeen.js
+++ src/screens/Restaurants/AddReviewRestaurantScreeen/AddReviewRestaurantScreeen.js
@@ -1,12 +1,53 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { useState, useEffect } from 'react';
+import { View } from 'react-native'
+import { Input, AirbnbRating, Button } from 'react-native-elements';
+import {
+  doc,
+  onSnapshot,
+} from 'firebase/firestore'
+import { db } from '../../../utils'
+import { styles } from './AddReviewRestaurantScreeen.styles';
 
 export function AddReviewRestaurantScreeen(props) {
-    const { route } = props;
-    console.log(route.params.idRestaurant);
+  const { route } = props;
+  const [restaurant, setRestaurant] = useState(null)
+
+
+  //console.log(route.params.idRestaurant);
   return (
     <View>
-      <Text>AddReviewRestaurantScreeen</Text>
+      <View style={styles.content}>
+        <View>
+          <Input
+            placeholder="Titulo"
+            containerStyle={styles.input}
+          />
+          <Input
+            placeholder="Comentario..."
+            multiline
+            inputContainerStyle={styles.comment}
+          />
+        </View>
+        <View style={styles.imgContent}>
+
+        </View>
+        <View style={styles.ratingContent}>
+          <AirbnbRating
+            count={5}
+            reviews={["Malo", "Regular", "Normal", "Muy Bueno", "Excelente"]}
+            defaultRating={0}
+            size={35}
+            onFinishRating={(value) => console.log(value)}
+          />
+        </View>
+      </View>
+      <View style={styles.btnContainer}>
+        <Button
+          title="Enviar Comentario"
+          buttonStyle={styles.btn}
+        />
+      </View>
     </View>
   )
 }
--- src/screens/Restaurants/AddReviewRestaurantScreeen/AddReviewRestaurantScreeen.styles.js
+++ src/screens/Restaurants/AddReviewRestaurantScreeen/AddReviewRestaurantScreeen.styles.js
@@ -1,4 +1,36 @@
 import { StyleSheet } from "react-native";
+import {colors} from "../../../utils/colors";
 
 export const styles = StyleSheet.create({
+    content: {
+        justifyContent: "space-between",
+        marginHorizontal: 20,
+    },
+    input: {
+        marginBottom: 10,
+        marginTop: 60,
+    },
+
+    comment: {
+        height: 100,
+        padding: 0,
+        margin: 0,
+    },
+    ratingContent: {
+        height: 40,
+        justifyContent: "center",
+        marginTop: 50,
+    },
+    imgContent: {
+        alignItems: "center",
+        marginVertical: 20,
+    },
+    btnContainer: {
+        marginTop: 80,
+        width: "95%",
+        alignSelf: "center",
+    },
+    btn: {
+        backgroundColor: colors.firstColor,
+    },
 });
