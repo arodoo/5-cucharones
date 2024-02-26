ad543d5efcb7ba4030850a59a4c43f95abf0294d
arodoo <haro.em@hotmail.com>
2024-02-26 10:51:07 -0600

    Add favorite functionality to BtnFavorite component

--- src/components/Restaurant/BtnFavorite/BtnFavorite.js
+++ src/components/Restaurant/BtnFavorite/BtnFavorite.js
@@ -1,22 +1,37 @@
 import React from 'react'
-import { View} from 'react-native'
-import { Icon} from 'react-native-elements';
+import { View } from 'react-native'
+import { Icon } from 'react-native-elements'
+import { getAuth } from 'firebase/auth'
+import { doc, setDoc, getDoc, query, where, collection, deleteDoc } from 'firebase/firestore'
+import uuid from 'react-native-uuid'
+import { db } from '../../../utils'
 import { styles } from './BtnFavorite.styles'
 
 export function BtnFavorite(props) {
-  const { idRestaurant } = props;
+  const { idRestaurant } = props
+  const auth = getAuth()
 
-  const addFavorite = () => {
-    console.log('Añadir a favoritos')
+  const addFavorite = async () => {
+    try {
+      const idFavorite = uuid.v4()
+      const data = {
+        id: idFavorite,
+        idRestaurant: idRestaurant,
+        idUser: auth.currentUser.uid
+      }
+      await setDoc(doc(db, 'favorites', idFavorite), data);
+    } catch (error) {
+      
+    }
   }
   return (
-    <View style = {styles.content}>
+    <View style={styles.content}>
       <Icon
         type='material'
         name='favorite'
         color='#DCDCDC'
         size={40}
-        
+
         onPress={() => addFavorite()}
       />
     </View>
