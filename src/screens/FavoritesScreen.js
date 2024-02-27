567d33486aa73f65542fe5b605907b8928cc93cd
arodoo <haro.em@hotmail.com>
2024-02-27 11:00:20 -0600

    dd favorites functionality

--- src/screens/FavoritesScreen.js
+++ src/screens/FavoritesScreen.js
@@ -1,12 +1,16 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { doc, setDoc, getDoc, query, where, collection, deleteDoc, onSnapshot } from 'firebase/firestore'
+import { db } from "../utils";
 import { UserNotLogged } from "../components/Favorites";
 
 
 export function FavoritesScreen() {
     const [hasLogged, setHasLogged] = useState(false)
+    const [favorites, setFavorites] = useState([])
     const auth = getAuth()
+    console.log(favorites);
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
@@ -14,15 +18,37 @@ export function FavoritesScreen() {
         }
         )
     }
-    , [])
+        , [])
 
-    if(!hasLogged) return (<UserNotLogged />)
+    useEffect(() => {
+        const q = query(
+            collection(db, 'favorites'),
+            where('idUser', '==', auth.currentUser.uid)
+        );
+        onSnapshot(q, async (snapshot) => {
+            let favoritesArray = [];
+            snapshot.forEach(async (item) => {
+                const data = item.data();
+                const docRef = doc(db, 'restaurants', data.idRestaurant);
+                const response = await getDoc(docRef);
+                const newData = response.data();
+                newData.id = data.id;
+                favoritesArray.push(newData);
+            });
+            setFavorites(favoritesArray);
+        }
+        )
+    }
+        , [hasLogged])
+
+
+
+    if (!hasLogged) return (<UserNotLogged />)
 
     return (
         <View >
-        <Text>Favorites</Text>
+            <Text>Favorites</Text>
         </View>
     );
-    }
+}
 
-    
