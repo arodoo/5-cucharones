73c95ede8c21f79efcfe1a8145e1328a25209080
arodoo <haro.em@hotmail.com>
2024-05-01 11:14:55 -0600

    Refactor RankingScreen component to fetch and display top-rated restaurants from Firestore

--- src/screens/RankingScreen.js
+++ src/screens/RankingScreen.js
@@ -1,10 +1,38 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { collection, query, orderBy, limit, onSnapshot } from 'firebase/firestore';
+import { size } from 'lodash';
+import { db } from "../utils";
+
 
 export function RankingScreen() {
+
+    const [restaurants, setRestaurants] = useState([]);
+    console.log(size(restaurants));
+
+    useEffect(() => {
+        const q = query(
+            collection(db, 'restaurants'),
+            orderBy('rating', 'desc'),
+            limit(10)
+        );
+
+        onSnapshot(q, (querySnapshot) => {
+            const restaurantsArray = [];
+            querySnapshot.forEach((doc) => {
+                const data = doc.data();
+                data.id = doc.id;
+                restaurantsArray.push(data);
+            });
+            setRestaurants(restaurantsArray);
+
+        });
+    }
+    , []);
+
     return (
         <View >
-        <Text>Ranking</Text>
+            <Text>Ranking</Text>
         </View>
     );
-    }
+}
