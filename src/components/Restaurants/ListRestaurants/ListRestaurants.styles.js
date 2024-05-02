ec64f5dbd3ea3df51604fb075f39edef03a10f88
arodoo <haro.em@hotmail.com>
2024-05-02 13:05:00 -0600

    Refactor ListRestaurants component styles to improve layout and add shadow effect

--- src/components/Restaurants/ListRestaurants/ListRestaurants.styles.js
+++ src/components/Restaurants/ListRestaurants/ListRestaurants.styles.js
@@ -2,12 +2,24 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     viewRestaurant: {
+        height: 100,
         flexDirection: "row",
-        margin: 10,
+        margin: 12,
+        backgroundColor: "#fff",
+        borderRadius: 8,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 10,
+        elevation: 10,
     },
     imageRestaurant: {
-        width: 80,
-        height: 80,
+        width: 90,
+        height: 100,
+        borderRadius: 5,
     },
     restaurantName: {
         fontWeight: "bold",
