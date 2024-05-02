563f07d6c63caccb5fd6cf7c41caf2833d46ac84
arodoo <haro.em@hotmail.com>
2024-05-02 12:51:18 -0600

    Refactor RestaurantRanking component to include image, title, rating, and description

--- src/components/Restaurants/RestaurantRanking/RestaurantRanking.js
+++ src/components/Restaurants/RestaurantRanking/RestaurantRanking.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, TouchableOpacity } from 'react-native'
+import { Image, Text, Rating, Icon } from 'react-native-elements'
+import { styles } from './RestaurantRanking.styles'
 
-export function RestaurantRanking() {
+export function RestaurantRanking(props) {
+  const { restaurant, index } = props
   return (
-    <View>
-      <Text>RestaurantRanking</Text>
-    </View>
+    <TouchableOpacity>
+      <View style={styles.content}>
+        <Image
+          resizeMode="cover"
+          PlaceholderContent={<Image source={require('../../../../assets/img/no-image.png')} style={styles.image} />}
+          source={{ uri: restaurant.images[0] }}
+          style={styles.image}
+        />
+        <View style={styles.titleRanking}>
+          <Text style={styles.title}>{restaurant.name}</Text>
+          <Rating imageSize={20} startingValue={restaurant.rating} readonly style={styles.rating} />
+        </View>
+        <Text style={styles.description}>{restaurant.description}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
--- src/components/Restaurants/RestaurantRanking/RestaurantRanking.styles.js
+++ src/components/Restaurants/RestaurantRanking/RestaurantRanking.styles.js
@@ -4,5 +4,49 @@ export const styles = StyleSheet.create({
     content: {
         flexDirection: 'column',
         flex: 1,
-    }
+        marginVertical: 10,
+        marginHorizontal: 15,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 10,
+        elevation: 10,
+    },
+
+    image: {
+        width: '100%',
+        height: 180,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    },
+
+    titleRanking: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 10,
+        marginHorizontal: 10,
+
+    },
+
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+
+    rating: {
+        marginTop: 10,
+
+    },
+
+    description: {
+        color: 'grey',
+        marginTop: 10,
+        marginHorizontal: 15,
+    },
+
 });
