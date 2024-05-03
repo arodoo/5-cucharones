a452c2d1ad72b2e9656d7b5aced6123101318448
arodoo <haro.em@hotmail.com>
2024-05-03 10:57:53 -0600

    chore: Refactor SearchScreen component to include search functionality and display search results

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,29 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, ScrollView } from "react-native";
+import { SearchBar, ListItem, Avatar, Icon } from "react-native-elements";
+import { Loading } from "../components/Shared";
 
 export function SearchScreen() {
+
+    const [search, setSearch] = useState("");
+    const [results, setResults] = useState([]);
+
     return (
-        <View >
-        <Text>Search</Text>
-        </View>
+        <>
+            <SearchBar placeholder="Buscar" lightTheme round
+                onChangeText={(text) => setSearch(text)}
+                value={search}
+            />
+            {!search ? <Loading show text="Cargando"></Loading> : <ScrollView>
+                <ListItem>
+                    <Avatar source={{ uri: "https://randomuser.me/api/port 0.2/nat=us" }} />
+                    <ListItem.Content>
+                        <ListItem.Title>Nombre</ListItem.Title>
+                        <ListItem.Subtitle>Descripción</ListItem.Subtitle>
+                    </ListItem.Content>
+                    <ListItem.Chevron />
+                </ListItem>
+            </ScrollView>}
+        </>
     );
-    }
-    
+}
